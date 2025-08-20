92b58cefb5ad632b1d3e56d0689249056a0ad28d
eltoncox <elton.unix@gmail.com>
2025-08-20 14:43:02 -0300

    Get game by id.

--- src/main/java/com/elton/devsuperior/dslinst/services/GameService.java
+++ src/main/java/com/elton/devsuperior/dslinst/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.elton.devsuperior.dslinst.dto.GameDTO;
 import com.elton.devsuperior.dslinst.dto.GameMinDTO;
 import com.elton.devsuperior.dslinst.entities.Game;
 import com.elton.devsuperior.dslinst.repositories.GameRepository;
@@ -13,11 +15,19 @@ import com.elton.devsuperior.dslinst.repositories.GameRepository;
 public class GameService {
 	
 	@Autowired
-	private GameRepository gameRepository;
+	private GameRepository gameRepository;	
 	
+	@Transactional(readOnly = true) // nao vou fazer nenhuma operação de escrita
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);		 
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){		
 		var result = gameRepository.findAll();		
 		return result.stream().map(x -> new GameMinDTO(x)).toList();	
 	}
+	
 
 }
